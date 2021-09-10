7830caa5fc1f1b29acbea151a12ae019be5277b4
zverkovavik <viktorya_zverkova@mail.ru>
2021-09-10 20:34:48 +0300

    Task solutions with reduce method

--- Untitled-2.js
+++ Untitled-2.js
@@ -10,8 +10,40 @@ const intDiff = (arr, n) => {
   return count;
 };
 
+// интерпретация array.includes()
 function check(a, x) {
   // your code here
   let result = a.filter(element => element === x);
   return !!result.length;
 }
+
+function positiveSum(arr) {
+  return arr.filter(element => element > 0).reduce((acc, val) => { 
+    return acc + val;
+  }, 0);
+}
+
+// Solution of Pandemia task https://www.codewars.com/kata/5e2596a9ad937f002e510435/
+function infected(s) {
+  let arr = s.split('');
+  if (arr.includes('X') && arr.includes('0') && arr.includes('1')) {
+    let infectedPeople = s.split('X').reduce((acc, val) => {
+      if (val.includes('1')) { 
+        return acc + val.length;
+      }
+      return acc;
+    }, 0);
+    
+    let summLength = s.split('X').join('').length;
+    return 100 * infectedPeople/summLength;
+  } else if(!arr.includes('X')) {
+    return arr.filter((element) => element === '1').length ? 100 : 0;
+  } else {
+     return 0; 
+  }
+}
+
+infected("01000000X000X011X0X"); 
+infected("01X000X010X011XX");
+infected("XXXXX");
+infected("0000000010");
