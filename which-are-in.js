65a475f0da92557988e4102cce4940f28b8e991b
zverkovavik <viktorya_zverkova@mail.ru>
2021-10-10 23:12:50 +0300

    Wich are in task has solved

---
+++ which-are-in.js
@@ -0,0 +1,31 @@
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+// Notes:
+// Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+// In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+// Beware: r must be without duplicates.
+
+function inArray(array1,array2){
+  let result = [];
+  let str = array2.join(' ');
+  array1.forEach((element) => {
+    if(str.includes(element)) {
+      result.push(element);
+    }
+  });
+  return result.sort();
+}
