5729923fb1c325c510adf8cdf2918ac46e7f9d5b
zverkovavik <viktorya_zverkova@mail.ru>
2021-10-12 23:52:46 +0300

    Palindrome with reg exp task has solved

---
+++ palindrome-with-reg-exp.js
@@ -0,0 +1,21 @@
+//Source: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+
+// Palindrome Checker
+// Return true if the given string is a palindrome. Otherwise, return false.
+
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+// Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+// We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+// We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+
+function palindrome(str) {
+  let regExp = /[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g;
+  let newStr = str.toLowerCase().replace(regExp, '');
+  return newStr === newStr.split('').reverse().join('') ;
+}
+
+palindrome("eye");
